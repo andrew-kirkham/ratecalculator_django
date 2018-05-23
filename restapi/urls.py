33c205afcca86131ff2f1aea5106391060936a9e
Andrew Kirkham <andrew.nicholas.kirkham@gmail.com>
2018-05-23 15:50:34 -0500

    adding swagger

--- restapi/urls.py
+++ restapi/urls.py
@@ -3,8 +3,12 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from restapi import views
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title = 'Coding Challenge')
 
 urlpatterns = [
+    url(r'^$', schema_view),
     url(r'^api/list/$', views.RateList.as_view()),
     url(r'^api/rate/$', views.RateQuery.as_view()),
 ]
