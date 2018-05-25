9793dcfaca4699d095ff5cb5bf67db03ef8d1f2f
Andrew Kirkham <andrew.nicholas.kirkham@gmail.com>
2018-05-25 10:12:47 -0500

    removing dead code

--- restapi/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.db import models
-
-class RateRequest(models.Model):
-    start = models.DateTimeField()
-    end = models.DateTimeField()
